597a52a8b9aa89b248cf242a502c448836cf6bb3
ThacHungNguyen <hungnt@lienviettech.com.vn>
2021-11-27 17:36:09 +0700

    UD

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-const menu = document.querySelector(".header__menu");
-
-menu.addEventListener("click", handleClickToggleMenu);
-
-function handleClickToggleMenu() {
-  const toggleMenu = document.querySelector(".toggle__menuList");
-  const menuCircle = document.querySelector(".page1__menu-circle");
-  const backgroundCircle = document.querySelector(
-    ".container__background-circle"
-  );
-  const headerMenu = document.querySelector(".header__menu");
-
-  toggleMenu.classList.toggle("active");
-  headerMenu.classList.toggle("active");
-  menuCircle.classList.toggle("active");
-  backgroundCircle.classList.toggle("active");
-}
-
-document
-  .querySelector(".footer__logo")
-  .addEventListener("click", handleClickLogoToTop);
-function handleClickLogoToTop() {
-  document.documentElement.scrollTop = 0;
-}
-
-
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  dots: true,
+  nav: true,
+  dotsEach: true,
+  autoplay: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      576:{
+          items:2
+      },
+      992:{
+          items:4
+      }
+  }
+})
